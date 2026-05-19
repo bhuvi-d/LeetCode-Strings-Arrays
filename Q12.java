dca9a29e03821d2f5bf4d67df946fa65c1774dd9
Bhuvaneshwari D <134070992+bhuvi-d@users.noreply.github.com>
2026-05-19 13:24:06 +0530

    Q12

---
+++ Q12.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length-1 ; i>=0;i--){
+            if (digits[i]+1 !=10){
+                digits[i] +=1;
+                return digits;
+            }
+            digits[i]=0;
+    }
+        int[] extened = new int[digits.length+1];
+        extened[0]=1;
+        return extened;
+
+
+    }
+}
