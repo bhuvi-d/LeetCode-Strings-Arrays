c78b36f149328ee833a7464a8ac50f3a8e11ad27
Bhuvaneshwari D <134070992+bhuvi-d@users.noreply.github.com>
2026-05-19 13:54:08 +0530

    Q13

---
+++ Q13.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int  x= 0;
+
+        for (int n : nums) {
+            x ^= n;
+        }
+
+        return x;        
+    }
+}
